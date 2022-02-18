7ecb297db450444f3f36729996d497e0edad095a
AbreuDProgrammer <leo.abreudp@gmail.com>
2022-02-18 21:27:02 +0000

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="mg.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script type="text/javascript" src="mg.js"></script>
+        <title>Memory Game</title>
+    </head>
+    <body>
+        <button id="start">Start</button>
+        <?php
+            $conexao = mysqli_connect('alunos.epcc.pt','al220038','epcc2020');
+        ?>
+    </body>
+</html>
